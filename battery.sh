468fc08b11f9c6f1e156073304ad4f736c3e19e7
Brian <brian@hewlett.home>
2021-12-29 18:18:11 -0600

    initial freebsd

--- battery.sh
+++ battery.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CAP_FILE="/sys/class/power_supply/BAT1/capacity"
-cat "$CAP_FILE"
+# CAP_FILE="/sys/class/power_supply/BAT1/capacity"
+# cat "$CAP_FILE"
+echo $(acpiconf -i 0 | grep "Remaining capacity" | cut -f 2)
 exit
-
--- status.sh
+++ status.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # TODO: try to implement a wlan interface status and connection SSID or local IP
 # https://unix.stackexchange.com/questions/352031/how-to-find-status-of-wlan0
@@ -7,8 +7,8 @@ status_str=
 separator=" | "
 while true; do
     status_str="BAT:"$(~/.config/sdorfehs/battery.sh)"$separator"
-    status_str="$status_str"$(date +'%A %b.%e, %Y %I:%M%p')"$separator"
-    status_str="$status_str"$(uptime -p)
+    status_str="$status_str"$(date +'%A %b.%e, %Y %I:%M%p')
+    # status_str="$status_str"$(uptime -p)
     echo "$status_str" > ~/.config/sdorfehs/bar
     sleep 5 
 done
