fb6671e6db6e3f82018f3a47c87b32794cf043d0
Brian Hoffpauir <bhoffpauir0424@gmail.com>
2022-08-14 18:48:17 -0500

    update power.sh & config

--- power.sh
+++ power.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+lockfile -r 0 /tmp/power.sh.lock || exit 1
+
 battery_status=100
 battery_state=charging
 
@@ -19,6 +21,9 @@ declare -r STATE_DISCHARGING="discharging"
 declare -r CRITICAL_WARNING=20
 declare -r CRITICAL_SUSPEND=15
 declare -r CRITICAL_HIBERNATE=10
+declare -r SHOW_CHARGED=90
+
+shown_once=0 # 0=false,1=true
 
 while :; do
     sleep $REFRESH_RATE
@@ -27,12 +32,20 @@ while :; do
     if [[ "$battery_state" == *dis* ]]; then
 	# echo "Discharging!"
         if [ "$battery_status" -le $CRITICAL_WARNING ]; then
-            sdorfehs -c "echo Warning: Battery state is low!"
+            sdorfehs -c "echo Warning: Battery state is low!" &
+	    notify-send --urgency=critical "Battery state is low!" &
         elif [ "$battery_status" -le $CRITICAL_SUSPEND ]; then
             loginctl suspend
         elif [ "$battery_status" -le $CRITICAL_HIBERNATE ]; then
             loginctl hibernate
         fi
+	shown_once=0
+    else
+	# if [ \( "$battery_status" -ge $SHOW_CHARGED \) -a \( $shown_once -eq 0 \) ]; then
+	   # shown_once=1
+	   # sdorfehs -c "echo Notice: Battery is fully charged!"
+	  #  notify-send --urgency=normal "Battery is fully charged!"
+	# fi
     fi
     #else
 	# echo "Charging!"
@@ -40,4 +53,6 @@ while :; do
     # echo $battery_status
     # echo $battery_state
 done
+
+# rm -f /tmp/power.sh.lock
 
