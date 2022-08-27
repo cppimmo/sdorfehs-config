6e612e77715537e8c0a3591525bc28ce7ec3f075
Brian Hoffpauir <bhoffpauir0424@gmail.com>
2022-08-27 15:02:06 -0500

    convert status script to perl

--- battery.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-CAP_FILE="/sys/class/power_supply/BAT0/capacity"
-cat "$CAP_FILE"
-exit
-
---
+++ status.pl
@@ -0,0 +1,74 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Readonly;
+
+# TODO: try to implement a wlan interface status and connection SSID or local IP.
+# NOTE: This file requires the Readonly package and brightnessctl.
+# https://unix.stackexchange.com/questions/352031/how-to-find-status-of-wlan0
+# Constants.
+Readonly my $HOME => $ENV{HOME};
+Readonly my $SEPARATOR => ' | ';
+Readonly my $UPTIME_SINCE_STRING => `uptime -s`;
+Readonly my $UPTIME_STRING => `date -d "$UPTIME_SINCE_STRING" '+UP:%d-%b-%Y %T' | tr a-z A-Z`;
+Readonly my $FIFO_BAR_FILE => "$HOME/.config/sdorfehs/bar";
+Readonly my $BATTERY_CAPACITY_FILE => "/sys/class/power_supply/BAT0/capacity";
+# "Globals".
+# my $barFile;
+my $batteryFile;
+
+sub Init {
+	# open(my $barFile, '>', "$FIFO_BAR_FILE") or die "Error: $!";
+	open($batteryFile, '<', "$BATTERY_CAPACITY_FILE") or die "Error: $!";
+}
+
+sub Quit {
+	# close($barFile);
+	close($batteryFile);
+}
+
+sub BatteryPercentage {
+	seek($batteryFile, 0, 0);
+	return <$batteryFile>;
+}
+
+sub BrightnessPercentage {
+	my $brightnessActual = `brightnessctl get`;
+	my $brightnessMax = `brightnessctl max`;
+	return ($brightnessActual / $brightnessMax) * 100.0;
+}
+
+sub DateTimeNow {
+	return `date +'T:%d-%b-%Y %T' | tr a-z A-Z`;
+}
+
+sub DoLoop {
+	while (1) {
+		my $statusString = '';
+
+		$statusString = sprintf('BAT:%02.0f%%', BatteryPercentage()) . $SEPARATOR;
+		$statusString = $statusString
+			. sprintf("BRT:%02.0f%%", BrightnessPercentage())
+			. $SEPARATOR;
+		$statusString = $statusString . DateTimeNow() . $SEPARATOR;
+		$statusString = $statusString . $UPTIME_STRING;
+		
+		# Remove newlines from string. Sdorfehs bar considers newlines as EOF.
+		$statusString =~ s/[\n]//g; 
+
+		# seek($barFile, 0, 0);
+		# print $barFile $statusString;
+		#print $statusString;
+		my $result = system("echo '$statusString' > $FIFO_BAR_FILE");
+		# truncate($barFile, tell($barFile));
+		sleep(1);
+	}
+	# close($barFile);
+	close($batteryFile);
+}
+
+Init();
+DoLoop();
+Quit();
+exit(0);
+
